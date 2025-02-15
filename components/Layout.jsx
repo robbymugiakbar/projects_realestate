2382e1649222264934cbe399733cb597c132f640
Hallo Robby <robbymugiakbar8@gmail.com>
2025-02-15 20:47:28 +0700

    First commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>Real Estate</title>
+        <title>HomeSphere</title>
       </Head>
       <Box maxWidth='1280px' m='auto'>
         <header>
--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+      'x-rapidapi-key': 'aa67842a5emsh7de2f2d177a94c9p15caa4jsn6ee1795151c6',
     },
   });
     
